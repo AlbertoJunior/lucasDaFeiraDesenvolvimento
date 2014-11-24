1a51843aa18046b3ddaf65699cdbfbb5b4c81719
Alberto Junior <alberto.junior.1995@gmail.com>
2014-11-24 13:35:41 -0300

    Alguns Scripts
    Andar e Exbir coletavel

---
+++ Scripts/Andar.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Andar : MonoBehaviour {
+	
+	public float jumpSpeed = 20f;
+	public float rotationSpeed = 13.5f;
+	public float moveSpeed = 6.5f;
+	public bool recarregando = false;
+	private Animator animacao;
+	
+	// Use this for initialization
+	void Start () {
+		animacao = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+		}
+		
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(Vector3.forward * -moveSpeed * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+			animacao.SetFloat("velocidade", moveSpeed);
+		}
+		if(Input.GetKey(KeyCode.Space) && !animacao.GetBool("pulando")){
+			transform.rigidbody.AddForce(0, jumpSpeed * (Time.deltaTime+10), 0);
+			animacao.SetBool("pulando", true);
+		}
+		if(Input.GetKey(KeyCode.F)){
+			animacao.SetTrigger("acenar");
+		}
+	}
+	
+	void FixedUpdate(){
+		if(animacao.GetFloat("velocidade") > 0.1){
+			animacao.SetFloat("velocidade",animacao.GetFloat("velocidade") * -Time.deltaTime);
+		}
+	}
+	
+	void OnCollisionEnter (Collision colisors){
+		foreach(ContactPoint col in colisors.contacts){
+			Debug.DrawRay(col.point, col.normal, Color.white);
+
+			if(col.point.Equals("Coletavel")){
+			}
+			if(col.point.Equals("Chao")){
+				animacao.SetBool("pulando", false);
+			}
+		}
+	}
+}
---
+++ Scripts/Coletavel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coletavel : MonoBehaviour {
+	public GameObject camera = null;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player")) {
+			Debug.Log("pega item ai mano");
+			camera.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag ("Player")) {
+			Debug.Log("volta ai mano");
+			camera.SetActive (false);
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.attachedRigidbody)
+			other.attachedRigidbody.AddForce(Vector3.up * 10);
+	}
+
+}
