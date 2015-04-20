9dae31e1c14f138dc3f16310b078ab23861b5b68
Alberto Junior <alberto.junior.1995@gmail.com>
2015-04-20 02:37:04 -0300

    Atualização no Script Andar

--- Scripts/Andar.cs
+++ Scripts/Andar.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class Andar : MonoBehaviour {
 	
-	public float jumpForce = 35f;
-	public float moveSpeed = 14f;
+	public float jumpForce;
+	public float moveSpeed;
+	public float distanciaRay;
+
 	private Animator animacao;
 	private bool isRight;
-	private Rigidbody rigibody;
+	private Ray rayCast;
 
 	// Use this for initialization
 	void Start () {
-		rigibody = GetComponent<Rigidbody>();
+		//rigibody = GetComponent<Rigidbody>();
 		animacao = GetComponent<Animator> ();
 		isRight = true;
 	}
@@ -23,23 +25,27 @@ public class Andar : MonoBehaviour {
 				flip (-180);
 				isRight = true;
 			}
-			transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
-			//rigibody.AddForce(transform.forward * moveSpeed, ForceMode.Acceleration);
-			animacao.SetFloat("velocidade", moveSpeed * Time.deltaTime);
+
+			if(verificaParede()){
+				transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+				animacao.SetFloat("velocidade", moveSpeed * Time.deltaTime);
+			}
 		}
 		else if (Input.GetKey (KeyCode.A)){
 			if(isRight){
 				flip (180);
 				isRight = false;
 			}
-			transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
-			//rigibody.AddForce(transform.forward * moveSpeed, ForceMode.Acceleration);
-			animacao.SetFloat("velocidade", moveSpeed * Time.deltaTime);
+
+			if(verificaParede()){
+				transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+				animacao.SetFloat("velocidade", moveSpeed * Time.deltaTime);
+			}
 		}
 
 		if(Input.GetKey(KeyCode.Space) && !animacao.GetBool("pulando")){
 			animacao.SetBool("pulando", true);
-			transform.GetComponent<Rigidbody>().AddForce(0, jumpForce * (Time.deltaTime+moveSpeed), 0);
+			transform.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 		}
 	}
 	
@@ -59,6 +65,25 @@ public class Andar : MonoBehaviour {
 
 	private void flip(float val){
 		transform.Rotate (0, val, 0);
+	}
+
+	private bool verificaParede(){
+		RaycastHit hit;
+
+		if (isRight) 
+			rayCast = new Ray (transform.position, Vector3.forward);
+		else 
+			rayCast = new Ray (transform.position, Vector3.back);
+
+		if(Physics.Raycast(rayCast, out hit, distanciaRay)){
+			if(hit.collider.tag == "Colisor"){
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		return true;
 	}
 
 }
